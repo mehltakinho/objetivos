866b499b261abaf18496fe8f82b559293de68ab8
mehltakinho <henrique.taques@escola.pr.gov.br>
2026-05-12 07:55:04 -0300

    n

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ const texto = document.querySelectorAll(".aba-conteudo")
 console.log(textos)
 
 for(let i = 0; i<botoes.length; i++){
-    
+    botoes[i].onclick = function (){
+        for(let j=0; j<botoes.length; j++){
+            botoes[j].classList.remove("ativo")
+            textos[j].classList.remove("ativo")
+        }
+        botoes[i].classList.remove("ativo")
+            textos[i].classList.remove("ativo")
+    }
 }
