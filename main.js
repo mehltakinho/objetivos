b683cbe0e721890cf3030eeacec878e93bfecc09
mehltakinho <henrique.taques@escola.pr.gov.br>
2026-05-12 07:49:27 -0300

    n

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const botoes = document.querySelectorAll(".botao")
 console.log(botoes)
 const texto = document.querySelectorAll(".aba-conteudo")
 console.log(textos)
+
+for(let i = 0; i<botoes.length; i++){
+    
+}
