1f8aae9347e1c69d9bfd54ed3ada751ce2731e8d
mehltakinho <henrique.taques@escola.pr.gov.br>
2026-05-12 07:55:25 -0300

    n

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ for(let i = 0; i<botoes.length; i++){
             botoes[j].classList.remove("ativo")
             textos[j].classList.remove("ativo")
         }
-        botoes[i].classList.remove("ativo")
-            textos[i].classList.remove("ativo")
+        botoes[i].classList.add("ativo")
+            textos[i].classList.add("ativo")
     }
 }
