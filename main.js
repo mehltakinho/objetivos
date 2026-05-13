e66543a355bb67aec7e4671a31acc3e1e33c88ed
mehltakinho <henrique.taques@escola.pr.gov.br>
2026-05-13 12:12:09 -0300

    n

--- main.js
+++ main.js
@@ -34,7 +34,11 @@ function calculaTempo(tempoObjetivo){
     segundos %= 60
     minutos %= 60
     horas %= 24
-    
+    if (tempoFinal > 0){
+        return[dias, horas, minutos, segundos]
+    }else {
+        return [0,0,0,0]
+    }
 }
 
 calculaTempo(tempoObjetivo1)
