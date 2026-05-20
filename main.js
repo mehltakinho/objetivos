8c83f57707f9bb1889f65f739dd800709d1dbe42
mehltakinho <henrique.taques@escola.pr.gov.br>
2026-05-20 12:23:10 -0300

    h

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const tempo = [tempoObjetivol, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4]
 
 function calculaTempo(tempoObjetivo){
     let tempoAtual = new Date ()
-    let tempoFinal = tempoObejtivo - tempoAtual
+    let tempoFinal = tempoObjetivo - tempoAtual
     console.org(tempoFinal)
     let segundos = Math.floor(tempoFinal / 1000)
     let minutos = Math.floor(segundos / 60)
